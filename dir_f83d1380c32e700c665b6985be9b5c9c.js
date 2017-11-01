3a2ea7f065e01db5c1f80ce3c816a1186ea229c0
Travis CI <travis@travis-ci.org>
2017-11-01 22:45:15 +0000

    Deploy code docs to GitHub Pages Travis build: 196
    Commit: 25265cf71ba74e286b827f04924a0863452a4b03

--- dir_f83d1380c32e700c665b6985be9b5c9c.js
+++ dir_f83d1380c32e700c665b6985be9b5c9c.js
@@ -1,6 +1,5 @@
 var dir_f83d1380c32e700c665b6985be9b5c9c =
 [
     [ "background.cpp", "background_8cpp.html", null ],
-    [ "cgal.cpp", "cgal_8cpp.html", null ],
-    [ "openmesh.cpp", "openmesh_8cpp.html", null ]
+    [ "cgal.cpp", "cgal_8cpp.html", null ]
 ];
--- search/typedefs_10.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['value_5ft',['value_t',['../classdependency__graph_1_1io_1_1basic__json.html#a35949a162279e40931eed51338d96a92',1,'dependency_graph::io::basic_json']]],
-  ['value_5ftype',['value_type',['../classdependency__graph_1_1io_1_1basic__json.html#a99faea85acba8757a8b6fe7a3e7bb235',1,'dependency_graph::io::basic_json::value_type()'],['../classdependency__graph_1_1io_1_1basic__json_1_1iter__impl.html#ae5a0a95bd3d9b94c5627c04a2ff1cabd',1,'dependency_graph::io::basic_json::iter_impl::value_type()']]]
-];
